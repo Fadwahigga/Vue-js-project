cabfd4b5736aaf3eaec5f0348774cd4d71ed6f43
Fadwa Ali <fadwa.ali@elevoro.com>
2023-05-10 20:23:19 +0200

    text binding by v-text

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div class="w-1/2 h-1/2 ">My Name Is Fadwa</div>
   <br/>
   <div class="w-1/2 h-1/2 ">{{name}}</div>
+  <br/>
+  <div class="w-1/2 h-1/2 " v-text="name"></div>
 </template>
 <script>
 export default {
