33b6b5a8544c9093b136852a7b0b25c268d07649
Fadwa Ali <fadwa.ali@elevoro.com>
2023-05-10 22:59:45 +0200

    reset

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script >
+export default {
+  data() { return { name: "fadwa" } }
+}
+</script>
+
+<template>
+  <div class="text-3xl font-bold underline bg-red-600">Hello {{ name }}</div>
+</template>
+
+
