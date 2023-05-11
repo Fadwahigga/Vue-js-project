997630ecdedc3031c248451e4fbc282514084673
Fadwa Ali <fadwa.ali@elevoro.com>
2023-05-11 13:08:27 +0200

    conditions

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
-<script >
-export default {
-  data() { return { name: "fadwa" } }
-}
-</script>
+
 
 <template>
-  <div class="text-3xl font-bold underline bg-red-600">Hello {{ name }}</div>
-  <div class="text-3xl font-bold underline bg-blue-500" v-text="name"></div>
+  <!-- <div class="text-3xl font-bold underline bg-red-600">Hello {{ name }}</div>
+  <div class="text-3xl font-bold underline bg-blue-500" v-text="name"></div> -->
+  <h1 v-if="num == 0">The Number is zero</h1>
+  <h1 v-else-if="num > 0">The Number is positive</h1>
+  <h1 v-else>The Number is not zero</h1>
+  <h1 v-else>The Number is not zero</h1>
 </template>
 
 
+<script >
+export default {
+  data() { return { 
+    // name: "fadwa" 
+    num :5
+  } }
+}
+</script>
