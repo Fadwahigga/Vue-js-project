6bc434ea8591d6c7c24fa43d3df74e322585a2ca
Fadwa Ali <fadwa.ali@elevoro.com>
2023-05-11 14:37:34 +0200

    v-for array of an array

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,11 @@
   <h1 v-else>The Number is not zero</h1>
   <h1 v-show="show">Show element</h1>
   <h1 v-for="name in names" :key="name">{{ name }}</h1> -->
-  <h1 v-for="name in fullNames" :key="name.frst">{{ name.frst }} {{ name.last }}</h1>
+  <!-- <h1 v-for="name in fullNames" :key="name.frst">{{ name.frst }} {{ name.last }}</h1> -->
+  <div v-for="actor in actors" :key="actor.name">
+    <h1>{{ actor.name }}</h1>
+    <h1 v-for="movie in actor.movies" :key="movie">{{ movie }}</h1>
+  </div>
 </template>
 
 
@@ -17,11 +21,12 @@
 export default {
   data() {
     return {
-      // name: "fadwa" 
+      // name: "fadwa" ,
       // num :-7,
-      // show:false
-      // names:["fadwa","ali","foff"]
-      fullNames: [{ frst: "fadwa", last: "ali" }, { frst: "ali", last: "fadl" }, { frst: "fadl", last: "higga" }]
+      // show:false,
+      // names:["fadwa","ali","foff"],
+      // fullNames: [{ frst: "fadwa", last: "ali" }, { frst: "ali", last: "fadl" }, { frst: "fadl", last: "higga" }],
+      actors: [{ name: "fadwa", movies: ["first", "secend"] }, { name: "Ali", movies: ["ali first", "ali secend"] }]
     }
   }
 }
