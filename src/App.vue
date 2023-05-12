c3adfd539f569cf509484d556089ab5ea45a54d2
Fadwa Ali <fadwa.ali@elevoro.com>
2023-05-12 15:19:10 +0200

    event handling

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
-
-
 <template>
-  <!-- <div class="text-3xl font-bold underline bg-red-600">Hello {{ name }}</div>
+<div class=" justify-center border p-60">  <!-- <div class="text-3xl font-bold underline bg-red-600">Hello {{ name }}</div>
   <div class="text-3xl font-bold underline bg-blue-500" v-text="name"></div> -->
   <!-- <h1 v-if="num == 0">The Number is zero</h1>
   <h1 v-else-if="num > 0">The Number is positive</h1>
@@ -15,9 +13,13 @@
     <h1 v-for="movie in actor.movies" :key="movie">{{ movie }}</h1>
   </div> -->
   <!-- <h1 v-for="(value , key ,index) in infVal" :key="value"> {{ index }} {{ key }}{{ value }}</h1> -->
-  <h1>{{ add ()}}</h1>
+  <!-- <h1>{{ add ()}}</h1>
   <h1>{{ multi() }}</h1>
-  <h1>{{ passdata(multvar) }}</h1>
+  <h1>{{ passdata(multvar) }}</h1> -->
+  <h1>{{ name }}</h1>
+  <button v-on:click="name = 'Ali'">Change name</button>
+  <h1>{{num}}</h1>
+  <button v-on:click="num = num +1">Increes</button></div>
 </template>
 
 
@@ -25,24 +27,24 @@
 export default {
   data() {
     return {
-      // name: "fadwa" ,
-      // num :-7,
+      name: "fadwa",
+       num :-7,
       // show:false,
       // names:["fadwa","ali","foff"],
       // fullNames: [{ frst: "fadwa", last: "ali" }, { frst: "ali", last: "fadl" }, { frst: "fadl", last: "higga" }],
       // actors: [{ name: "fadwa", movies: ["first", "secend"] }, { name: "Ali", movies: ["ali first", "ali secend"] }]
       // infVal: { name: "fadwa", chanel: "fadwaaa", course: "vuejs" }
-      multvar:3
+      multvar: 3
     }
   },
-  methods:{
-    add(){
-      return 5+5
+  methods: {
+    add() {
+      return 5 + 5
     },
-    multi(){
-      return this.multvar*2
+    multi() {
+      return this.multvar * 2
     }
-    ,passdata(a){
+    , passdata(a) {
       return a;
     }
   }
